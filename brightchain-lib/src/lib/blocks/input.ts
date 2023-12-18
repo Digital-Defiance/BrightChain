538f5c51b243f837d88148cc2075634b2357ff42
Jessica Mulein <jessica@digitaldefiance.org>
2023-12-18 19:41:11 +0000

    start differentiating block types

--- brightchain-lib/src/lib/blocks/input.ts
+++
@@ -1,6 +0,0 @@
-import { BlockType } from "../enumerations/blockType"
-import { EphemeralBlock } from "./ephemeral"
-
-export class InputBlock extends EphemeralBlock {
-  public readonly blockType: BlockType = BlockType.Input;
-}
--- brightchain-lib/src/lib/enumerations/blockType.ts
+++ brightchain-lib/src/lib/enumerations/blockType.ts
@@ -1,8 +1,9 @@
 export enum BlockType {
-  ConstituentBlockList = 'CBL',
-  EphemeralReconstitutedBlock = 'ERB',
-  Input = 'IB',
-  InputConstituentBlockList = 'ICBL',
-  OwnerFreeWhitenedBlock = 'OFWB',
-  Unknown = 'UNK',
+  Unknown = -1,
+  OwnerFreeWhitenedBlock = 0,
+  Random = 1,
+  RawData = 2,
+  FECData = 3,
+  OwnedDataBlock = 4,
+  ConstituentBlockList = 5,
 }
