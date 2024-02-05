0568bc4f10e80ef2b4fd97a1714fcd7dc40cfc4d
Jessica Mulein <jessica@digitaldefiance.org>
2024-02-04 22:43:30 -0800

    wip (#8)
    * move inmemory statics to statichelpers.tuple
    
    * metadata to/from disk
    
    * new xormultipletransform stream class
    
    * refactor async service xor function
    
    * new cblStream, async store xors to baseblock
    
    * update cblstream

--- brightchain-lib/src/lib/blocks/memoryTuple.ts
+++ brightchain-lib/src/lib/blocks/memoryTuple.ts
@@ -1,10 +1,6 @@
 import { TupleSize } from "../constants";
 import { ChecksumBuffer } from "../types";
 import { BaseBlock } from "./base";
-import { EncryptedOwnedDataBlock } from "./encryptedOwnedData";
-import { OwnedDataBlock } from "./ownedData";
-import { RandomBlock } from "./random";
-import { WhitenedBlock } from "./whitened";
 
 export class InMemoryBlockTuple {
     public static readonly TupleSize = TupleSize;
@@ -34,32 +30,5 @@ export class InMemoryBlockTuple {
     public static fromIds(blockIDs: ChecksumBuffer[], fetchBlock: (blockId: ChecksumBuffer) => BaseBlock): InMemoryBlockTuple {
         const blocks = blockIDs.map((blockId) => fetchBlock(blockId));
         return new InMemoryBlockTuple(blocks);
-    }
-    public static xorSourceToPrimeWhitened(sourceBlock: BaseBlock | EncryptedOwnedDataBlock, whiteners: WhitenedBlock[], randomBlocks: RandomBlock[]): WhitenedBlock {
-        let block: BaseBlock = sourceBlock;
-        for(let i=0;i<whiteners.length;i++) {
-            block = block.xor<WhitenedBlock>(whiteners[i]);
-        }
-        for(let i=0;i<randomBlocks.length;i++) {
-            block = block.xor<WhitenedBlock>(randomBlocks[i]);
-        }
-        return new WhitenedBlock(block.blockSize, block.data, block.dateCreated, block.id);
-    }
-    public static makeTupleFromSourceXor(sourceBlock: BaseBlock | EncryptedOwnedDataBlock, whiteners: WhitenedBlock[], randomBlocks: RandomBlock[]): InMemoryBlockTuple {
-        const primeWhitenedBlock = this.xorSourceToPrimeWhitened(sourceBlock, whiteners, randomBlocks);
-        const tuple = new InMemoryBlockTuple([primeWhitenedBlock, ...whiteners, ...randomBlocks]);
-        return tuple;
-    }
-    public static xorDestPrimeWhitenedToOwned(primeWhitenedBlock: WhitenedBlock, whiteners: WhitenedBlock[]): OwnedDataBlock {
-        let block: BaseBlock = primeWhitenedBlock;
-        for(let i=0;i<whiteners.length;i++) {
-            block = block.xor<BaseBlock>(whiteners[i]);
-        }
-        return new OwnedDataBlock(block.blockSize, block.data, block.lengthBeforeEncryption, block.dateCreated, block.id);
-    }
-    public static makeTupleFromDestXor(primeWhitenedBlock: WhitenedBlock, whiteners: WhitenedBlock[]): InMemoryBlockTuple {
-        const ownedDataBlock = this.xorDestPrimeWhitenedToOwned(primeWhitenedBlock, whiteners);
-        const tuple = new InMemoryBlockTuple([ownedDataBlock, ...whiteners]);
-        return tuple;
     }
 }
